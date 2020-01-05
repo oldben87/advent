5ddeeddb284686cfa29684a6aadbaf78e829e177
Ben Webb <b-webb@outlook.com>
2020-01-05 09:37:30 +0000

    day 4 part 1

--- day4.js
+++ day4.js
@@ -1,34 +1,75 @@
 
-let i = 100000;
-let pmatch = 0;
+let i = 136777;
+let pmatch = [];
 let date1 = new Date();
-let endNumber
+
+while ( i < 589999 ) {
+
+    let i1 = parseInt(i.toString().charAt(0))
+    let i2 = parseInt(i.toString().charAt(1))
+    let i3 = parseInt(i.toString().charAt(2))
+    let i4 = parseInt(i.toString().charAt(3))
+    let i5 = parseInt(i.toString().charAt(4))
+    let i6 = parseInt(i.toString().charAt(5))
+
+   let numRep = 0;
+ 
+        if(i2 < i1) { 
+            i2 = i1;
+            i3 = i1;
+            i4 = i1;
+            i5 = i1;
+            i6 = i1;
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+          } else if ( i3 < i2 ) {
+            i3 = i2;
+            i4 = i2;
+            i5 = i2;
+            i6 = i2;
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+
+          } else if ( i4 < i3 ) {
+            i4 = i3;
+            i5 = i3;
+            i6 = i3;        
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+
+
+          } else if ( i5 < i4 ) {
+            i5 = i4;
+            i6 = i4;
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+
+          } else if ( i6 < i5 ) {
+            i6 = i5;
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+
+
+          } else if (i1 === i2 || i2 === i3 ||  i3===i4 || i4===i5 || i5===i6) {
+            numRep = parseInt(i1.toString() + i2.toString() + i3.toString() + i4.toString() + i5.toString() + i6.toString())
+            i = numRep
+            pmatch.push(i)
+          }
+
+        
+
+		i++
+}
+console.log(pmatch.length)
+console.log(pmatch[pmatch.length - 1])
+
 
 /*
-
-const myVar = 1234;
-String(myVar).charAt(1)
-//"2"
-parseInt(String(myVar).charAt(1))
-//2
-
-*/
-
-for (i=100000; i>500001; i++) {
-
-    let i1 = i.toString().charAt(0)
-    let i2 = i.toString().charAt(1)
-    let i3 = i.toString().charAt(2)
-    let i4 = i.toString().charAt(3)
-    let i5 = i.toString().charAt(4)
-    let i6 = i.toString().charAt(5)
-    
-    endNumber = i1 + i2 + i3 + i4 + i5 + i6
-
-}
-
-
-
 if (i == 500000) {
     date2 = new Date();
 
@@ -49,7 +90,9 @@ if (i == 500000) {
 
 
 console.log('Hours: ' + hh + ' Mins: '+ mm + ' Secs: ' + ss);
-console.log(endNumber);
+console.log(pmatch);
 
 
 }
+
+*/
